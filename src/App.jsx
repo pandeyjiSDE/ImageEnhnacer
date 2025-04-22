64783e1e90c18124015dcbbe8043ffb2eeca02be
pandeyjiSDE <pandeyji6602@gmail.com>
2025-04-22 22:36:31 +0530

    ImageEnhancer

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./components/Home";
+const App = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4 ">
+            <div className="text-center mb-8">
+                <h1 className="text-5xl font-bold text-gray-800 mb-2">
+                    AI Image Enhancer{" "}
+                </h1>
+                <p className="text-lg text-gray-500">
+                    Get Your Enhance Version!
+                </p>
+            </div>
+
+            <Home />
+
+            <div className="text-sm text-gray-500 mt-6">
+                @AI Powered Image Enhancer
+            </div>
+        </div>
+    );
+};
+
+export default App;
+
